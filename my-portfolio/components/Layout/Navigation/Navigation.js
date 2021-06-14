cdeb892a01e4ac3fec38d074bc81870ac5c9a2cd
Dusan Djordjic <77781108+DusanDjordjic@users.noreply.github.com>
2021-06-14 18:34:41 +0200

    Navigation finished

--- my-portfolio/components/Layout/Navigation/Navigation.js
+++ my-portfolio/components/Layout/Navigation/Navigation.js
@@ -1,7 +1,37 @@
+import classes from "./Navigation.module.scss";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
+import Link from "next/link";
+import Image from "next/image";
 const Navigation = () => {
   return (
-    <section>
-      <h2>Navigation</h2>
+    <section className={classes.navigation}>
+      <div className={classes.logo}>
+        <Image src="/logo.png" height="30" width="30" />
+        <p>Portfolio</p>
+      </div>
+      <ul className={classes.linksContainer}>
+        <li>Projects</li>
+        <li>Technologies</li>
+        <li>About</li>
+      </ul>
+      <ul>
+        <li>
+          <Link href="https://github.com/DusanDjordjic">
+            <FaGithub />
+          </Link>
+        </li>
+        <li>
+          <Link href="https://www.linkedin.com/in/du%C5%A1an-%C4%91or%C4%91i%C4%87-689110205/">
+            <FaLinkedin />
+          </Link>
+        </li>
+        <li>
+          <Link href="https://www.instagram.com/dusan_spl/">
+            <RiInstagramFill />
+          </Link>
+        </li>
+      </ul>
     </section>
   );
 };
