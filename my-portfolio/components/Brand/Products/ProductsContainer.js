146a2b08c85fa57657a7b2fea346788ce0fefa5a
Dusan Djordjic <77781108+DusanDjordjic@users.noreply.github.com>
2021-06-20 10:48:12 +0200

    slider drag and drop compleated

--- my-portfolio/components/Brand/Products/ProductsContainer.js
+++ my-portfolio/components/Brand/Products/ProductsContainer.js
@@ -1,10 +1,10 @@
 import classes from "./ProductsContainer.module.scss";
 import SingleProduct from "./SingleProduct";
 import { useEffect, useState, useRef } from "react";
-import Link from "next/link";
-import Image from "next/image";
 const ProductsContainer = () => {
   const [mouseDown, setMouseDown] = useState(false);
+  const [touchDown, setTouchDown] = useState(false);
+  const [firstTouchX, setFirstTouchX] = useState(0);
   const [mouseX, setMouseX] = useState(0);
   const [moveX, setMoveX] = useState(0);
   const [sliderX, setSliderX] = useState(0);
@@ -13,6 +13,7 @@ const ProductsContainer = () => {
   const container = useRef();
   const handleMouseDown = (e) => {
     const x = e.pageX;
+    console.log(x);
     setMouseX(x);
     setMouseDown(true);
   };
@@ -29,37 +30,62 @@ const ProductsContainer = () => {
   useEffect(() => {
     if (mouseDown) {
       window.addEventListener("mousemove", handleMouseMove);
+      // window.addEventListener("touchmove", handleMouseMove);
     }
     if (!mouseDown) {
       setSliderX(sliderMovedX);
       window.removeEventListener("mousemove", handleMouseMove);
+      // window.removeEventListener("touchmove", handleMouseMove);
     }
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
+      // window.removeEventListener("touchmove", handleMouseMove);
     };
   }, [mouseDown]);
 
   useEffect(() => {
     slider.current.style.transform = `translateX(${sliderMovedX}px)`;
   }, [sliderMovedX]);
-
   useEffect(() => {
-    // if (sliderX + moveX < 0) {
-    //   setSliderMovedX(0);
-    // } else if (sliderX + moveX > slider.current.scrollWidth) {
-    //   setSliderMovedX(slider.current.scrollWidth);
-    // } else {
+    window.addEventListener("mouseup", () => {
+      setMouseDown(false);
+    });
 
-    // }
+    return () => {
+      window.removeEventListener("mouseup", () => {
+        setMouseDown(false);
+      });
+    };
+  }, []);
+  useEffect(() => {
     let newX = sliderX + moveX;
     if (newX < -slider.current.scrollWidth + container.current.scrollWidth) {
       newX = -slider.current.scrollWidth + container.current.scrollWidth;
     } else if (newX > 0) {
       newX = 0;
     }
-
     setSliderMovedX(newX);
   }, [moveX]);
+
+  const handleTouchMove = (e) => {
+    const touchX = e.changedTouches[0].pageX;
+    console.log(e.changedTouches[0]);
+    let newX = sliderX + touchX - firstTouchX;
+    if (newX < -slider.current.scrollWidth + container.current.scrollWidth) {
+      newX = -slider.current.scrollWidth + container.current.scrollWidth;
+    } else if (newX > 0) {
+      newX = 0;
+    }
+    setSliderMovedX(newX);
+  };
+  const handleTouchDown = (e) => {
+    setFirstTouchX(e.changedTouches[0].pageX);
+    setTouchDown(true);
+  };
+  const handleTouchUp = (e) => {
+    setSliderX(sliderMovedX);
+    setTouchDown(false);
+  };
 
   return (
     <div ref={container}>
@@ -68,6 +94,10 @@ const ProductsContainer = () => {
           ref={slider}
           onMouseDown={(e) => handleMouseDown(e)}
           onMouseUp={(e) => handleMouseUp(e)}
+          onTouchStart={(e) => handleTouchDown(e)}
+          onTouchEnd={(e) => handleTouchUp(e)}
+          onTouchMove={(e) => handleTouchMove(e)}
+          style={mouseDown ? { cursor: "grabbing" } : { cursor: "grab" }}
         >
           <li>
             <SingleProduct
